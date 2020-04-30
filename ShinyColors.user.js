90ace1965917c395dcd58336eebd760b7cc6b081
biuuu <biuuu@users.noreply.github.com>
2020-04-30 09:08:03 +0000

    Deploy to GitHub pages

---
+++ ShinyColors.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         偶像大师ShinyColors汉化
+// @namespace    https://github.com/biuuu/ShinyColors
+// @version      0.11.2
+// @description  提交翻译或问题请到 https://github.com/biuuu/ShinyColors
+// @icon         https://shinycolors.enza.fun/icon_192x192.png
+// @author       biuuu
+// @match        https://shinycolors.enza.fun/*
+// @run-at       document-end
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @connect      api.interpreter.caiyunai.com
+// @connect      translate.google.cn
+// @connect      fanyi.baidu.com
+// @updateURL    https://www.shiny.fun/ShinyColors.user.js
+// @supportURL   https://github.com/biuuu/ShinyColors/issues
+// ==/UserScript==
+(function () {
+	'use strict';
+
+	const ENVIRONMENT = "";
+	    const DEV = false;
+	    const SHOW_UPDATE_TEXT = false;
+	    const COLLECT_CARD_RATE = false;
+
+
+
+}());
